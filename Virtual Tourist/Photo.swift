dfc258e2a466ddfc1e3ecf24427efca55caf4ec1
Troy Tobin <troy.d.tobin@gmail.com>
2015-08-15 00:49:08 +0930

    Add url and data to the managed photo entity

--- Virtual Tourist/Photo.swift
+++ Virtual Tourist/Photo.swift
@@ -13,12 +13,16 @@ import CoreData
 class Photo : NSManagedObject {
   
   struct Keys {
-    static let id  = "id"
+    static let id   = "id"
+    static let url  = "url"
+    static let data = "data"
   }
   
   ///Set the Photo attributes to Core data attributes
-  @NSManaged var id: String
-  @NSManaged var pin: Pin
+  @NSManaged var id:  String
+  @NSManaged var url:  String
+  @NSManaged var data: NSData
+  @NSManaged var pin:  Pin
   
   /// initialise the Core data
   override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
@@ -37,7 +41,9 @@ class Photo : NSManagedObject {
     super.init(entity: entity,insertIntoManagedObjectContext: context)
     
     // Set the photos attributes
-    id  = dictionary[Keys.id] as! String
+    id   = dictionary[Keys.id]  as! String
+    url  = dictionary[Keys.url]  as! String
+    data = dictionary[Keys.data] as! NSData
   }
 }
 
